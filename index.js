2b7791d3ff4de257554b293ebfba1ed08fb77140
mwangisam203 <mwangisam203@gmail.com>
2023-09-29 14:15:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (const name of names) {
+      thankYouMessages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
